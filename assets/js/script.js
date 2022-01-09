133f96b0e3de34f0d003ebc2f50d09b4c035764e
Kerry Long <victory173451@gmail.com>
2022-01-08 23:30:13 -0500

    first pass at moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,50 @@
 var savedEvents ;
 
+function timeAudit() {
+
+    var amTime = [9,10,11];
+    var pmTime = [12,1,2,3,4,5];
+    var calTime;
+    var amORpm;
+    var curTime = moment().format("MM-DD-YYY h:mm a");
+    var theDate = moment().format("MM-DD-YYYY").toString();
+    console.log(theDate);
+
+    console.log("curTime is " + curTime);
+    $("textarea").each(function () {
+        
+        if (jQuery.inArray(parseInt(this.getAttribute("data-hour")), amTime) !== -1) {
+            amORpm = "AM";
+        }
+        else {
+            amORpm = "PM";
+        }
+        calTime = (this.getAttribute("data-hour")).toString() + ':00' + amORpm;
+        console.log("This is calTime before moment " + calTime);
+
+        calTime = moment(theDate + " " + calTime, 'MM-DD-YYYY h:mm a').format('MM-DD-YYYY h:mm a');
+        console.log("CalTime after date " + calTime);
+        
+        
+        //calTime = moment(theDate + " " + calTime).format("h:mm  a")
+        console.log("calTime is " + calTime);
+
+        console.log(this);
+
+        if (calTime < curTime) {
+            $(this).addClass("past");
+        }
+        else if (calTime > curTime) {
+            $(this).addClass("future");
+        }
+        else {
+            $(this).addClass("present");
+        }
+
+
+    })
+}
+
 
 function loadEvents() {
 
@@ -49,10 +94,12 @@ function storeData() {
         savedEvents.push(timeSlot);
         localStorage.setItem('calEvents', JSON.stringify(savedEvents));
     }
-    
+
 }
 
 $("#calendar").on("click", "textarea", saveData);
 $("#calendar").on("click", "button", storeData);
+$("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
 loadEvents();
+timeAudit();
 
